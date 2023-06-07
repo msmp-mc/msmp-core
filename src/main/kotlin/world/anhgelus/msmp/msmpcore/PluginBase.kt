f94f16322ce7fa56c8d16cb68d8b09dfbd163119
Anhgelus Morhtuuzh <anhgelus.morhtuuzh@gmx.com>
2023-06-07 22:06:46 +0200

    fix(pluginbase): not init logger and instance

--- src/main/kotlin/world/anhgelus/msmp/msmpcore/PluginBase.kt
+++ src/main/kotlin/world/anhgelus/msmp/msmpcore/PluginBase.kt
@@ -11,23 +11,21 @@ abstract class PluginBase: JavaPlugin() {
     abstract fun disable()
 
     override fun onEnable() {
-        // init vars
+        INSTANCE = this
+        LOGGER = logger
+
         enable()
         LOGGER.info("$pluginName has been enabled!")
     }
 
     override fun onDisable() {
         disable()
-        LOGGER.info("$pluginName has been disabled!")
+        LOGGER.info("$pluginName is being disabled!")
     }
 
     open class CompanionBase {
         lateinit var INSTANCE: PluginBase
         lateinit var LOGGER: Logger
-
-        fun getInstance(): PluginBase {
-            return INSTANCE
-        }
     }
 
     companion object : CompanionBase()
